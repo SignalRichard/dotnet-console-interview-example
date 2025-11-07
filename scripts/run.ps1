70b7817a4000286a98a7214960a0dad6d7114daf
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2025-11-07 08:34:05 -0500

    fix: run.ps1 policy path

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -3,12 +3,12 @@ param(
 )
 
 if([string]::IsNullOrEmpty($ApplicationFilePath)) {
-    $ApplicationFilePath = (Join-Path -Path $PSScriptRoot -ChildPath '../src/RemoveSingleInstanceCharacters/bin/Debug/net8.0/RemoveSingleInstanceCharacters' -AdditionalChildPath $(if($IsWindows) { @('exe') } else { @() }))
+    $ApplicationFilePath = (Join-Path -Path $PSScriptRoot -ChildPath "../src/RemoveSingleInstanceCharacters/bin/Debug/net8.0/RemoveSingleInstanceCharacters$(if($IsWindows) { '.exe' } else { [string]::Empty })")
 }
 
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath 'NoSingleInstanceCharacters')
 # Build-Solution -SolutionFilePath (Join-Path -Path $PSScriptRoot -ChildPath '../RemoveSingleInstanceCharacters.sln')
-$opaProcess = Start-OpenPolicyAgent -PolicyDirectoryPath (Join-Path -Path $PSScriptRoot -ChildPath '../policy')
+$opaProcess = Start-OpenPolicyAgent -PolicyDirectoryPath (Join-Path -Path $PSScriptRoot -ChildPath '../policy/no_single_instance_characters')
 (Import-Tests -TestsFilePath (Join-Path -Path $PSScriptRoot -ChildPath 'test_arguments.json')).tests | Test-Application -ApplicationFilePath $ApplicationFilePath
 $opaProcess | Stop-Process
 Remove-Module -Name 'NoSingleInstanceCharacters'

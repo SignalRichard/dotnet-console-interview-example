e3e986c5756930fe0f822faf264002d57c2bdac8
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2025-11-07 11:01:01 -0500

    docs: add TLA+ ref spec to C# implementation

--- src/RemoveSingleInstanceCharacters/Extensions.cs
+++ src/RemoveSingleInstanceCharacters/Extensions.cs
@@ -4,6 +4,13 @@ namespace RemoveSingleInstanceCharacters
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Remove instances of single characters from the str.
+        /// See <a href="https://github.com/SignalRichard/dotnet-console-interview-example/blob/main/spec/RemoveSingleInstanceCharacters/RemoveSingleInstanceCharacters.tla">RemoveSingleInstanceCharacters.tla</a> for reference specification.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="characters"></param>
+        /// <returns></returns>
         public static string RemoveSingleInstanceCharacters(this string str, IEnumerable<string> characters)
         {
             StringBuilder stringBuilder = new();
